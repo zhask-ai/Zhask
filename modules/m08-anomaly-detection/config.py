dbfaca90fa9a7d9536301cd74760c7ff8b1b5fc9
Dishanth234 <dishanthk02@gmail.com>
2026-04-18 20:21:26 -0400

    M08

--- modules/m08-anomaly-detection/config.py
+++ modules/m08-anomaly-detection/config.py
@@ -2,8 +2,8 @@
 import os
 
 REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379")
-INPUT_STREAM: str = os.getenv("INPUT_STREAM", "analyzed_events")
-OUTPUT_STREAM: str = os.getenv("OUTPUT_STREAM", "anomaly_events")
+INPUT_STREAM: str = os.getenv("INPUT_STREAM", "integrishield:traffic_flow_events")
+OUTPUT_STREAM: str = os.getenv("OUTPUT_STREAM", "integrishield:anomaly_scores")
 CONSUMER_NAME: str = os.getenv("CONSUMER_NAME", "m08-anomaly-detection")
 GROUP_NAME: str = os.getenv("GROUP_NAME", "m08-consumers")
 MODEL_PATH: str = os.getenv("MODEL_PATH", "/app/ml/models/isolation_forest_v1.pkl")
