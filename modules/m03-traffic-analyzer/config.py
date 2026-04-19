2f1181f7c5b8ebf5ce717b0de6943adc3754c93b
Dishanth234 <dishanthk02@gmail.com>
2026-04-18 20:21:19 -0400

    M03

--- modules/m03-traffic-analyzer/config.py
+++ modules/m03-traffic-analyzer/config.py
@@ -2,8 +2,8 @@
 import os
 
 REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379")
-INPUT_STREAM: str = os.getenv("INPUT_STREAM", "rfc_events")
-OUTPUT_STREAM: str = os.getenv("OUTPUT_STREAM", "analyzed_events")
+INPUT_STREAM: str = os.getenv("INPUT_STREAM", "integrishield:api_call_events")
+OUTPUT_STREAM: str = os.getenv("OUTPUT_STREAM", "integrishield:traffic_flow_events")
 CONSUMER_NAME: str = os.getenv("CONSUMER_NAME", "m03-traffic-analyzer")
 GROUP_NAME: str = os.getenv("GROUP_NAME", "m03-consumers")
 LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
